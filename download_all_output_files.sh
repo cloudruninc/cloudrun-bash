309810cb77a382fe4400043d78cda31cd932928c
Milan Curcic <caomaco@gmail.com>
2019-04-09 13:57:23 -0400

    re-use existing scripts when downloading files in bulk

--- download_all_output_files.sh
+++ download_all_output_files.sh
@@ -10,14 +10,12 @@ fi
 source set-env.sh
 
 run_id=$1
-url=${CLOUDRUN_API_URL}/runs/$run_id
 
-files=$(curl -s ${url} -H "${CLOUDRUN_AUTH_HEADER}" | \
+files=$(./get_run.sh $run_id | \
         python3 -c "import sys, json; [print(f['name']) \
     	            for f in json.load(sys.stdin)['output_files']]")
 
 for file in ${files}; do
   echo Downloading ${file}..
-  url=${CLOUDRUN_API_URL}/runs/${run_id}/output/$file
-  curl $url -H "${CLOUDRUN_AUTH_HEADER}" --output $file
+  ./download_output_file.sh $run_id $file
 done
